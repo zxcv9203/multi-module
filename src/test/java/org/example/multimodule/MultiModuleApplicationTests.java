ff94522c74f5845ec519aa05b920c115787f84cb
yongckim <zxcv9203@naver.com>
2024-03-01 23:08:05 +0900

    :tada: 프로젝트 초기화

---
+++ src/test/java/org/example/multimodule/MultiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.multimodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
