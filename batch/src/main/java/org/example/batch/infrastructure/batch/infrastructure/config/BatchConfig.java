64884cd68ad3576d93e473d60117b31a28fb641c
yongckim <zxcv9203@naver.com>
2024-03-15 20:12:01 +0900

    :truck: 패키지 구조 변경

--- batch/src/main/java/org/example/batch/infrastructure/batch/infrastructure/config/BatchConfig.java
+++ batch/src/main/java/org/example/batch/infrastructure/batch/infrastructure/config/BatchConfig.java
@@ -1,4 +1,4 @@
-package org.example.batch.infrastructure.batch.config;
+package org.example.batch.infrastructure.batch.infrastructure.config;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.context.annotation.Configuration;
