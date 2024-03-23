9a5627159102134c4f8f8cffcbb07e9069eb4e59
yongckim <zxcv9203@naver.com>
2024-03-23 22:44:54 +0900

    :bug: 컴포넌트 스캔의 대상에 core 모듈도 추가

--- batch/src/main/java/org/example/batch/BatchApplication.java
+++ batch/src/main/java/org/example/batch/BatchApplication.java
@@ -3,7 +3,10 @@ package org.example.batch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "org.example.core",
+        "org.example.batch"
+})
 public class BatchApplication {
 
     public static void main(String[] args) {
